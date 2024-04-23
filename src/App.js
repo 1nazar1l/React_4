7899a5a5ed74e6bd5dd9673e62b473c46a31c837
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-04-23 19:01:28 +0300

    update code

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [toCurrency, setToCurrency] = useState("USD")
   const [fromPrice, setFromPrice] = useState(0)
   const [toPrice, setToPrice] = useState(0)
+
+
   useEffect(() => { 
     async function fetchData(){
       const response = await fetch("https://www.cbr-xml-daily.ru/latest.js")
@@ -17,25 +19,28 @@ function App() {
     }
     fetchData()
   }, []);
+
+  
   useEffect(() => {
     onChangeFromPrice(Number(fromPrice))
   },[fromCurrency, fromPrice])
-  // useEffect(() => {
-  //   onChangeToPrice(Number(toPrice))
-  // },[toCurrency, toPrice])
+
+
   function onChangeFromPrice(value){
     let prize = fromCurrency == "RUB" ? value*rates[toCurrency] : value/rates[fromCurrency]
     let result = toCurrency == "RUB" ? prize*Rub_RATES : prize*rates[toCurrency]
     setToPrice(toCurrency == "RUB" || fromCurrency == "RUB" ? prize : result)
     setFromPrice(value)
   }
+
+
   function onChangeToPrice(value){
     let result = rates[fromCurrency]/rates[toCurrency]*value
     setFromPrice(result)
     setToPrice(value)
   }
-  console.log(fromPrice)
-  console.log(toPrice)
+
+  
   return (
     <div className="App">
       <Block onChangeValue={onChangeFromPrice} value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency} />
